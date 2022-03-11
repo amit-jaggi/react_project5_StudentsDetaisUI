b1f3ddb0f3d2912edde7042a14347eacc19c181e
amit-jaggi <amitjaggi.official@gmail.com>
2022-03-11 12:30:33 +0530

    assessment-5 uploading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header'
+import Home from './Components/Home'
+import Student from './Components/Student'
+import Contact from './Components/Contact'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import AddStudent from './Components/AddStudent'
+import { StudentAPI } from './Components/StudentContext'
+import UpdateStudent from './Components/UpdateStudent'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StudentAPI>
+      <div className="App">
+        <Router>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Navigate to='/home' />} />
+            <Route path='/home' element={<Home />} />
+            <Route exact path='/student' element={<Student />} />
+            <Route path='/student/addStudent' element={<AddStudent />} />
+            <Route path='/student/edit/:ID' element={<UpdateStudent />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/*' element={<Navigate to='/home' />} />
+          </Routes>
+        </Router>
+      </div>
+    </StudentAPI>
   );
 }
 
